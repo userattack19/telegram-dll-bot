c868651078c1863d0309f7eee313d97adec722e6
userattack19 <zen1don22@gmail.com>
2026-03-15 10:18:50 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -9,7 +9,7 @@ from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, FSInputFile
 
-TOKEN = "8749394795:AAETQPuVtGNYzUhPvh8UDyPyv-t0EuVXKwU"
+TOKEN = "8749394795:AAG9HkJM3_wBephFCLCt8uOWhnmm11TOXJQ"
 ADMIN_ID = int(os.getenv("ADMIN_ID") or 6667142324)
 
 USERS_FILE = "users.txt"
